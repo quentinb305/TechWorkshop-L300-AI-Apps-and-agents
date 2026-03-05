a756bc8dc3bf7ac3c28c85cda2c20200bc53605e
Quentin Brasseur <qubrasse@microsoft.com>
2026-03-05 15:28:05 +0100

    red teaming

---
+++ src/app/agents/redTeamingAgent_initializer.py
@@ -0,0 +1,45 @@
+# Azure imports
+from azure.identity import DefaultAzureCredential
+from azure.ai.evaluation.red_team import RedTeam, RiskCategory, AttackStrategy
+from pyrit.prompt_target import OpenAIChatTarget
+import os
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+# Azure AI Project Information
+azure_ai_project = os.getenv("FOUNDRY_ENDPOINT")
+
+# Instantiate your AI Red Teaming Agent
+red_team_agent = RedTeam(
+    azure_ai_project=azure_ai_project,
+    credential=DefaultAzureCredential(),
+    risk_categories=[
+        RiskCategory.Violence,
+        RiskCategory.HateUnfairness,
+        RiskCategory.Sexual,
+        RiskCategory.SelfHarm
+    ],
+    num_objectives=5,
+)
+
+# Configuration for Azure OpenAI model
+chat_target = OpenAIChatTarget(
+    model_name=os.environ.get("gpt_deployment"),
+    endpoint=f"{os.environ.get("gpt_endpoint")}/openai/deployments/{os.environ.get('gpt_deployment')}/chat/completions" ,
+    api_key=os.environ.get("gpt_api_key"),
+)
+
+async def main():
+    red_team_result = await red_team_agent.scan(
+        target=chat_target,
+        scan_name="Red Team Scan - Easy-Moderate Strategies",
+        attack_strategies=[
+            AttackStrategy.Flip,
+            AttackStrategy.ROT13,
+            AttackStrategy.Base64,
+            AttackStrategy.AnsiAttack,
+            AttackStrategy.Tense
+        ])
+
+asyncio.run(main())
